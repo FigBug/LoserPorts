1f84db06c494f35d7b9a556e65904fde1a30608f
Roland Rabien <figbug@gmail.com>
2022-10-19 09:52:26 -0700

    StereoEnhancer

---
+++ plugins/StereoEnhancer/Source/PluginEditor.cpp
@@ -0,0 +1,33 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AudioProcessorEditor::AudioProcessorEditor (AudioProcessor& p)
+    : gin::ProcessorEditor (p), proc (p)
+{
+    extraProgrammer = "Michael \"LOSER\" Gruhn";
+
+    for (auto pp : p.getPluginParameters())
+    {
+        auto pc = new gin::Knob (pp);
+        
+        addAndMakeVisible (pc);
+        controls.add (pc);
+    }
+    
+    setGridSize (7, 1);
+}
+
+AudioProcessorEditor::~AudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioProcessorEditor::resized()
+{
+    gin::ProcessorEditor::resized();
+
+	auto x = 2;
+	for (auto pp : proc.getPluginParameters())
+		componentForParam (*pp)->setBounds (getGridArea (x++, 0));
+}
